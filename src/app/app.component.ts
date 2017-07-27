767ccbe791c8db2e26b569bc0f80f09489eefc15
offwork <chomunist@gmail.com>
2017-07-27 13:58:37 +0300

    first change of the senior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  myList;
+
+  ngOnInit(): void {
+    this.myList = [
+      {label: "Tour of Heroes", link: "https://angular.io/tutorial"},
+      {label: "CLI Documentation", link: "https://github.com/angular/angular-cli/wiki"},
+      {label: "Angular blog", link: "http://angularjs.blogspot.ca/"},
+    ];
+  }
 }
